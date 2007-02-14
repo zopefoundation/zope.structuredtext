d70cd29abe6b8a4e25668064cf06fc214e11c81e
Alexander Heavner <alex@zope.com>
2007-02-14 21:17:43 +0000

    Changed version number in setup.py. Used find_packages to find packages instead of listing them out. Removed setup.cfg.in, INSTALL.txt, develop.py, CHANGES.txt, test.py, MANIFEST.in and README.txt because they are no longer needed. Changed the try, catch statement in __init__.py

--- setup.py
+++ setup.py
@@ -18,13 +18,10 @@ $Id$
 
 import os
 
-try:
-    from setuptools import setup, Extension
-except ImportError, e:
-    from distutils.core import setup, Extension
+from setuptools import setup, find_packages
 
 setup(name='zope.structuredtext',
-      version='3.3-dev',
+      version='3.3dev',
       url='http://svn.zope.org/zope.structuredtext',
       license='ZPL 2.1',
       description='XXX',
@@ -32,7 +29,7 @@ setup(name='zope.structuredtext',
       author_email='zope3-dev@zope.org',
       long_description='',
       
-      packages=['zope', 'zope.structuredtext'],
+      packages=find_packages('src'),
       package_dir = {'': 'src'},
 
       namespace_packages=['zope',],
