70b1bead554e78cf38714aae4ca094d835131a2d
Tres Seaver <tseaver@palladion.com>
2014-12-29 12:28:28 -0500

    svb

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ long_description = (
 
 setup(
     name='zope.structuredtext',
-    version='4.1.0',
+    version='4.1.1.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description='StructuredText parser',
